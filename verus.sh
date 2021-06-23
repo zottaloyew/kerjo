d411fb52fd56f4348f4261536fd278c1aff5581b
zottaloyew <85932742+zottaloyew@users.noreply.github.com>
2021-06-23 05:27:33 +0200

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -4,4 +4,7 @@ sudo apt install screen -y
 sudo apt install cpulimit
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
+while [1]; do
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RTVEd4W2ejcZnizFz5QoiBdfruNA4cuZFd.sp -p x --cpu 2
+sleep 30
+done
