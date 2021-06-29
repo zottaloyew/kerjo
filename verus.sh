c97e1907f1e0cea565225d7730cab04d26164754
zottaloyew <85932742+zottaloyew@users.noreply.github.com>
2021-06-29 15:54:28 +0200

    Delete verus.sh

--- verus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
-tar xf xmrig-6.12.2-linux-x64.tar.gz
-cd xmrig-6.12.2
-while [1]; do
-./xmrig --donate-level 1 -o gulf.moneroocean.stream:10128 -u 8AkrfutMaVHaYaMB4y9oSXdQv7fAsrcMCCpydbw7Mfin2F2w9QGAwnSSMFm3H2bGbtYBntsinuiTyHdjP8gqwGZTVgRUGPy -p db -a rx/0 -k
-sleep 30
-done
